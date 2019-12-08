787555798b337073a5ca4343ff7e0998ac00268c
snapeeek <maciekwozniak97@gmail.com>
2019-12-08 15:10:49 +0100

    Okno niemodalne module2.py

--- dialog.py
+++ dialog.py
@@ -1,56 +1,72 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from module import Module
 
+from module import Module
+from module2 import KoloDialog
 
 class Dialog(QMainWindow):
     fileName = ""
 
     def __init__(self):
         super(Dialog, self).__init__()
-        self.big_widget = QWidget()
-        self.color = QColor(0, 255, 0, 255)
-        self.current_color = 'green'
         self.dialog = Module(self)
+        self.currentColor = 'green'
+        self.color = QColor(0, 255, 0, 255)
+        self.bigWidget = QWidget()
         self.color_menu = QAction("Kolor okna głównego", self)
-        self.color_menu.setCheckable(True)
+        self.outside_circle =100
+        self.inside_circle = 50
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle('Dialogi')
         self.setGeometry(400, 400, 800, 400)
         grid = QGridLayout()
-        
         self.setWindowIcon(QIcon('happyface.jpg'))
-        self.setCentralWidget(self.big_widget)
-        
+        self.setCentralWidget(self.bigWidget)
         palette = QPalette()
         palette.setColor(QPalette.Window, self.color)
-       
         self.setPalette(palette)
-        
         self.dialog.hide()
         self.dialog.changed.connect(self.changed_color)
 
-        self.color_menu.triggered.connect(self.toggle_title_dialog)
+
+        self.color_menu.setCheckable(True)
+        self.color_menu.triggered.connect(self.toggle_dialog)
 
         circle_set = QAction("Ustawienia koła", self)
+        circle_set.triggered.connect(self.get_sth)
 
-        menubar = self.menuBar()
-        file_menu = menubar.addMenu('&Dialog')
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('&Dialog')
         file_menu.addAction(self.color_menu)
         file_menu.addAction(circle_set)
 
-        self.big_widget.setLayout(grid)
+        self.bigWidget.setLayout(grid)
 
         self.show()
 
-    def toggle_title_dialog(self):  # funkcja obsługi elementu menu /pokaż – schowaj dialog
+    def get_sth(self):
+        popout = KoloDialog()
+        popout.setModal(True)
+        popout.setGeometry(400,400,550,300)
+        popout.setWindowTitle("Ustawianie Kola")
+        popout.setup_values(self.outside_circle,self.inside_circle)
+        result = popout.exec()
+        ret = popout.get_result()
+        self.outside_circle = int(ret[0])
+        self.inside_circle = int(ret[1])
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_ellipses(qp)
+        qp.end()
+
+    def toggle_dialog(self):  # funkcja obsługi elementu menu /pokaż – schowaj dialog
         if self.color_menu.isChecked():
             self.dialog.show()
-            self.dialog.color = self.current_color
-        else:            
+            self.dialog.color = self.currentColor
+        else:
             self.dialog.hide()
 
     def changed_color(self, color):
@@ -58,25 +74,25 @@ class Dialog(QMainWindow):
             self.color = QColor(0, 255, 0, 255)
             palette = QPalette()
             palette.setColor(QPalette.Window, self.color)
-            self.current_color = 'green'
+            self.currentColor = 'green'
             self.setPalette(palette)
         elif color == 'red':
             self.color = QColor(255, 0, 0, 255)
             palette = QPalette()
             palette.setColor(QPalette.Window, self.color)
-            self.current_color = 'red'
+            self.currentColor = 'red'
             self.setPalette(palette)
         elif color == 'blue':
             self.color = QColor(0, 0, 255, 255)
             palette = QPalette()
             palette.setColor(QPalette.Window, self.color)
-            self.current_color = 'blue'
+            self.currentColor = 'blue'
             self.setPalette(palette)
         else:
             self.color = QColor(255, 255, 0, 255)
             palette = QPalette()
             palette.setColor(QPalette.Window, self.color)
-            self.current_color = 'green'
+            self.currentColor = 'green'
             self.setPalette(palette)
 
     def paintEvent(self, e):
@@ -91,14 +107,12 @@ class Dialog(QMainWindow):
         qp.setPen(col)
         qp.setBrush(QColor(112, 112, 112, 255))
 
-        main_size = self.big_widget.frameSize()
-        main_height = main_size.height()
-        main_width = main_size.width()
-        point = QPoint(main_width / 2, main_height / 2)
-        qp.drawEllipse(point, 100, 100)
+        main_size = self.bigWidget.frameSize()
+        point = QPoint(int(main_size.width() / 2),int(main_size.height() / 2))
+        qp.drawEllipse(point, self.outside_circle, self.outside_circle)
 
         qp.setBrush(self.color)
-        qp.drawEllipse(point, 50, 50)
+        qp.drawEllipse(point, self.inside_circle, self.inside_circle)
 
 
 if __name__ == '__main__':
---
+++ module2.py
@@ -0,0 +1,158 @@
+from PyQt5.QtCore import Qt, QRect, QPoint, pyqtSignal
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class KoloDialog(QDialog):
+    zmiana = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super(KoloDialog, self).__init__(parent)
+        self.setGeometry(400, 300, 150, 150)
+        self.outside_circle_beg = 0
+        self.inside_circle_beg = 0
+        # ten layout na cale okienko
+        big_layout = QVBoxLayout()
+
+        # widget na suwaki i podglad
+        widget_grid = QWidget()
+        grid_layout = QGridLayout()
+        widget_grid.setLayout(grid_layout)
+        widget_grid.setGeometry(150, 150, 150, 150)
+        widget_grid.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        object_validate = QIntValidator(self)
+        object_validate.setRange(0, 255)
+        self.zew = QLabel()
+        self.zew.setText("Na Zewnatrz")
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMaximum(255)
+        self.slider.setMinimum(0)
+        self.slider.valueChanged.connect(self.slider1_change)
+        self.line_edit = QLineEdit('0')
+        self.line_edit.setValidator(object_validate)
+        self.line_edit.textChanged.connect(self.line_edit1_change)
+
+        self.wew = QLabel()
+        self.wew.setText("Wewnatarz")
+        self.slider2 = QSlider(Qt.Horizontal)
+        self.slider2.setMaximum(255)
+        self.slider2.setMinimum(0)
+        self.slider2.valueChanged.connect(self.slider2_change)
+        self.line_edit2 = QLineEdit('0')
+        self.line_edit2.setValidator(object_validate)
+        self.line_edit2.textChanged.connect(self.line_edit2_change)
+
+        # podglad
+        self.color = QColor(240, 240, 240, 255)
+        self.podglad = QLabel()
+        self.podglad.setGeometry(400, 410, 412, 460)
+
+        # dodawanie sliderow labeli pol edycyjnych i podgladu do grida
+        grid_layout.addWidget(self.zew, 0, 0)
+        grid_layout.addWidget(self.slider, 0, 1)
+        grid_layout.addWidget(self.line_edit, 0, 2)
+        grid_layout.addWidget(self.wew, 1, 0)
+        grid_layout.addWidget(self.slider2, 1, 1)
+        grid_layout.addWidget(self.line_edit2, 1, 2)
+        grid_layout.addWidget(self.podglad, 0, 3)
+
+        # przyciski do aktywacji
+        widgetnaprzyciski = QWidget()
+        layoutnaprzyckiski = QHBoxLayout()
+        self.ok = QPushButton()
+        self.ok.setText("Ok")
+        self.ok.pressed.connect(self.close)
+        self.cancel = QPushButton()
+        self.cancel.setText("Anuluj")
+        self.cancel.pressed.connect(self.revert_changes)
+        layoutnaprzyckiski.addStretch(1)
+        layoutnaprzyckiski.addWidget(self.ok)
+        layoutnaprzyckiski.addWidget(self.cancel)
+        widgetnaprzyciski.setLayout(layoutnaprzyckiski)
+
+        # dodawnie tych wudgetow z layoutami
+        big_layout.addWidget(widget_grid)
+        big_layout.addWidget(widgetnaprzyciski)
+        self.setLayout(big_layout)
+        self.setModal(True)
+        self.show()
+
+    def paintEvent(self, *args, **kwargs):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_ellipses(qp)
+        qp.end()
+        
+    def draw_ellipses(self, qp):
+        col = QColor(0, 0, 0)
+        col.setNamedColor('#d4d4d4')
+        qp.setPen(col)
+        qp.setBrush(QColor(112, 112, 112, 255))
+
+        mainSize = self.podglad.rect()
+        mainHeight = mainSize.height()
+        mainWidth = mainSize.width()
+        point = QPoint(int(mainWidth / 2) + 400, int(mainHeight / 2) + 70)
+
+        qp.drawEllipse(point, int(self.slider.value() * 0.3), int(self.slider.value() * 0.3))
+        qp.setBrush(self.color)
+        qp.drawEllipse(point, int(self.slider2.value() * 0.3), int(self.slider2.value() * 0.3))
+
+    def revert_changes(self):
+        self.slider.setValue(self.outside_circle_beg)
+        self.slider2.setValue(self.inside_circle_beg)
+        self.close()
+
+    def line_edit1_change(self, value):
+        if not value:
+            return
+        self.slider.setValue(int(value))
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_ellipses(qp)
+        qp.end()
+
+    def slider1_change(self, value):
+        self.line_edit.setText(str(value))
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_ellipses(qp)
+        qp.end()
+
+    def line_edit2_change(self, value):
+        if not value:
+            return
+        self.slider2.setValue(int(value))
+
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_ellipses(qp)
+        qp.end()
+        
+    def slider2_change(self, value):
+        self.line_edit2.setText(str(value))
+
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_ellipses(qp)
+        qp.end()
+        
+    def setup_values(self, outside_circle, inside_circle):
+        if not outside_circle:
+            return
+        if not inside_circle:
+            return
+        self.outside_circle_beg = outside_circle
+        self.inside_circle_beg = inside_circle
+        self.slider.setValue(int(outside_circle))
+        self.line_edit.setText(str(outside_circle))
+        self.slider2.setValue(int(inside_circle))
+        self.line_edit2.setText(str(inside_circle))
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_ellipses(qp)
+        qp.end()
+
+    def get_result(self):
+        return [self.slider.value(), self.slider2.value()]
