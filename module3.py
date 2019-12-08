b768037e3fcdd3049650662d7e41b446ef3d6ba3
snapeeek <maciekwozniak97@gmail.com>
2019-12-08 17:30:49 +0100

    Obrazek

--- module3.py
+++ module3.py
@@ -8,14 +8,23 @@ class FaceDialog(QDialog):
     def __init__(self, parent=None):
         super(FaceDialog, self).__init__(parent)
         self.setGeometry(400, 300, 150, 150)
-        self.setFixedSize(400,300)
+        self.setFixedSize(320,300)
         self.setWindowTitle("Zmiana ikony")
-        self.radio_buttons = QButtonGroup()
+
         self.progress = QProgressBar()
         self.progress.setOrientation(Qt.Vertical)
         self.progress.setMaximum(100)
         self.progress.setMinimum(0)
         self.progress.setValue(50)
+
+        self.pictures = ['happyface.jpg', 'normalface.png', 'sadface.png']
+        self.picture = QLabel()
+        self.picture.setPixmap(QPixmap(self.pictures[0]))
+        self.actual_picture = self.pictures[0]
+
+        pix_map = QPixmap(self.actual_picture).scaled(90, 90, Qt.KeepAspectRatio)
+        self.picture.setPixmap(pix_map)
+
         big_layout = QVBoxLayout()
 
         # widget na suwaki i podglad
@@ -25,10 +34,12 @@ class FaceDialog(QDialog):
         widget_grid.setGeometry(150, 150, 150, 150)
         widget_grid.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
 
+
         radio_button1 = QRadioButton("wesola")
         radio_button1.setChecked(True)
         radio_button2 = QRadioButton("normalna")
         radio_button3 = QRadioButton("smutna")
+        self.radio_buttons = QButtonGroup()
         self.radio_buttons.addButton(radio_button1, 1)
         self.radio_buttons.addButton(radio_button2, 2)
         self.radio_buttons.addButton(radio_button3, 3)
@@ -80,6 +91,7 @@ class FaceDialog(QDialog):
         push_buttons_widget.setLayout(push_buttons_widget_layout)
 
         grid_layout.addWidget(button_widget1, 0, 0)
+        grid_layout.addWidget(self.picture, 0, 1)
         grid_layout.addWidget(push_buttons_widget, 0, 2)
         grid_layout.addWidget(button_widget2, 1, 0)
         grid_layout.addWidget(help_widget, 1, 1)
